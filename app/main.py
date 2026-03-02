afdba782db847f99b4de6c42946953569eee632d
Normis-Skuf <test-email@mail.com>
2026-03-02 16:37:05 +0200

    developed distance class

--- app/main.py
+++ app/main.py
@@ -1,3 +1,61 @@
+from __future__ import annotations
+
+
 class Distance:
-    # Write your code here
-    pass
+
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: Distance | int) -> Distance:
+        if not isinstance(other, Distance):
+            other = Distance(other)
+        return Distance(
+            self.km + other.km
+        )
+
+    def __iadd__(self, other: Distance | int) -> Distance:
+        if not isinstance(other, Distance):
+            other = Distance(other)
+        self.km = self.km + other.km
+        return self
+
+    def __mul__(self, other: Distance | int) -> Distance:
+        return Distance(
+            self.km * other
+        )
+
+    def __truediv__(self, other: Distance | int) -> Distance:
+        return Distance(
+            round((self.km / other), 2)
+        )
+
+    def __lt__(self, other: Distance | int) -> bool:
+        if not isinstance(other, Distance):
+            other = Distance(other)
+        return self.km < other.km
+
+    def __gt__(self, other: Distance | int) -> bool:
+        if not isinstance(other, Distance):
+            other = Distance(other)
+        return self.km > other.km
+
+    def __eq__(self, other: Distance | int) -> bool:
+        if not isinstance(other, Distance):
+            other = Distance(other)
+        return self.km == other.km
+
+    def __le__(self, other: Distance | int) -> bool:
+        if not isinstance(other, Distance):
+            other = Distance(other)
+        return self.km <= other.km
+
+    def __ge__(self, other: Distance | int) -> bool:
+        if not isinstance(other, Distance):
+            other = Distance(other)
+        return self.km >= other.km
